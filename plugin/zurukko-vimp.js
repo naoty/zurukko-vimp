a84a6378db7f8ef18e7514a4cd22bb6b2ef8555d
Naoto Kaneko <naoty.k@gmail.com>
2014-01-15 21:43:57 +0900

    Initial commit

---
+++ plugin/zurukko-vimp.js
@@ -0,0 +1,30 @@
+var INFO = xml`
+<plugin name="zurukko-vimp.js"
+        version="0.1"
+        summary="Read current page with zurukko"
+        href="http://github.com/naoty/zurukko-vimp"
+        xmlns="http://vimperator.org/namespaces/liberator">
+
+        <author email="naoty.k@gmail.com">Naoto Kaneko</author>
+        <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
+        <project name="Vimperator" minVersion="3.3"/>
+        <p>Read current page with zurukko</p>
+        <item>
+            <tags>"zurukko"</tags>
+            <spec>:zurukko</spec>
+            <description>
+                <p>Read current page with zurukko</p>
+            </description>
+        </item>
+</plugin>`;
+
+(function () {
+    var name = "zurukko";
+    var desc = "Read current page with zurukko";
+    var command = function (args) {
+        var zurukkoHost = location.host + ".zurukko.jp";
+        location.href = location.protocol + zurukkoHost + location.pathname;
+    };
+
+    commands.addUserCommand([name], desc, command);
+})();
